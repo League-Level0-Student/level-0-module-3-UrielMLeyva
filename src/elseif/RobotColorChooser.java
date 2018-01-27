a3575f243a21d8c1dfe44c0678ad265a5e8de3bc
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-01-27 10:55:20 -0800

    Robot Color Chooser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -12,27 +12,40 @@ import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-
+Robot r2d2=new Robot();
 		//3. ask the user what color they would like the robot to draw
 		String color=JOptionPane.showInputDialog("Enter a color");
 		//4. use an if/else statement to set the pen color that the user requested
 		if(color.equals("red")){
-			r2d2.setPenColor()
+		r2d2.setPenColor(Color.red);
 		}
+		else if(color.equals("green")) {
+				r2d2.setPenColor(Color.green);
+			}
+			else if(color.equals("blue")) {
+				r2d2.setPenColor(Color.blue);
+			}
+		
         //5. if the user doesn’t enter anything, choose a random color
-
+			
         //6. put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//2. set the pen width to 10
 		r2d2.setPenWidth(10);
 	    //1. make the robot draw a shape (this will take more than one line of code)
- 		Robot r2d2=new Robot();
-		r2d2.move(70);
-		r2d2.turn(120);
-		r2d2.move(70);
-		r2d2.turn(120);
-		r2d2.move(70);
-		r2d2.turn(120);
-			
+ 		r2d2.setSpeed(25);
+		r2d2.turn(180);
+		r2d2.move(250);
+		r2d2.penDown();
+		r2d2.turn(90);
+		r2d2.move(400);
+		r2d2.turn(90);
+		r2d2.move(450);
+		r2d2.turn(90);
+		r2d2.move(600);
+		r2d2.turn(90);
+		r2d2.move(450);
+		r2d2.turn(90);
+		r2d2.move(450);
 	}
 }
